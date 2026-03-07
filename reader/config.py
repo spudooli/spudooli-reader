e5296a531c2bbe4c3bd07e42daa8d19eb8b1c136
root <root@spudooli.com>
2026-03-07 13:10:36 +1300

    The session cookie will now actually be marked Secure, HttpOnly, and SameSite=Lax

--- reader/config.py
+++ reader/config.py
@@ -1,6 +1,6 @@
 from reader import app
 
 app.config['SECRET_KEY'] = 'supersecretpassphrasehere'
-SESSION_COOKIE_SECURE = True   
-SESSION_COOKIE_HTTPONLY = True 
-SESSION_COOKIE_SAMESITE = 'Lax'
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
